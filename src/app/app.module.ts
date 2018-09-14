8824b3b3b5c19cb64ea3e098bb79d6abc4b68c70
Wess58 <wesleymbate@gmail.com>
2018-09-14 14:30:11 +0300

    created quotes-details components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule
