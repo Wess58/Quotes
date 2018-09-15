47a8f6ecc137fb69044565502d009555151c567e
Wess58 <wesleymbate@gmail.com>
2018-09-15 12:37:29 +0300

    created Form Submition function

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,19 +7,26 @@ import { Quotes } from '../quotes'
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-          // create the quotes array, plural since the array properties
-        quotes = [
-          new Quotes (1,'Happiness is when what you think, what you say and what you do are in harmony.','Mahatma Gandhi','Mbate w.g'),
-          new Quotes (2,'Happiness is when what you think, what you say and what you do are in harmony.','Mahatma Gandhi','Mbate w.g'),
-          new Quotes (3,'Happiness is when what you think, what you say and what you do are in harmony.','Mahatma Gandhi','Mbate w.g'),
-          new Quotes (4,'Happiness is when what you think, what you say and what you do are in harmony.','Mahatma Gandhi','Mbate w.g'),
+  // create the quotes array, plural since the array properties
+  quotes = [
+    new Quotes(1, 'Happiness is when what you think, what you say and what you do are in harmony.', 'Mahatma Gandhi', 'Mbate w.g'),
+    new Quotes(2, 'Happiness is when what you think, what you say and what you do are in harmony.', 'Mahatma Gandhi', 'Mbate w.g'),
+    new Quotes(3, 'Happiness is when what you think, what you say and what you do are in harmony.', 'Mahatma Gandhi', 'Mbate w.g'),
+    new Quotes(4, 'Happiness is when what you think, what you say and what you do are in harmony.', 'Mahatma Gandhi', 'Mbate w.g'),
 
-        ]
-        toogleDetails(index){
-          this.quotes[index].showDescription = !this.quotes[index].showDescription;
-        }
+  ]
+  addNewQuotes(quotes) {
+    let quotesLength = this.quotes.length;
+    quotes.id = quotesLength + 1;
+    this.quotes.push(quotes)
+
+  }
+
+  toogleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
   constructor() { }
-          //ngOnInit is a life cycle hook. Life cycle hooks are called to show different stages in the life cycle of a component
+  //ngOnInit is a life cycle hook. Life cycle hooks are called to show different stages in the life cycle of a component
   ngOnInit() {
   }
 
