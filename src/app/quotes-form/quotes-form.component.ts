9b6db09ba62debac11f7fccdd694c6092e21b4d5
Wess58 <wesleymbate@gmail.com>
2018-09-15 12:36:24 +0300

    created Form Submition function

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quotes } from '../quotes'
 
 @Component({
@@ -8,6 +8,11 @@ import { Quotes } from '../quotes'
 })
 export class QuotesFormComponent implements OnInit {
           newQuotes=new Quotes(0,"","","");
+          @Output() addQuotes=new EventEmitter<Quotes>();
+
+          submitQuotes(){
+            this.addQuotes.emit(this.newQuotes);
+          }
   constructor() { }
 
   ngOnInit() {
