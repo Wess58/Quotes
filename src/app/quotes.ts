5aa7f200aafed753d6b49923b9caa2aa36513c5f
Wess58 <wesleymbate@gmail.com>
2018-09-16 17:39:29 +0300

    Added vote properties to the quotes Array

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,7 @@
 export class Quotes {
   // defines properties of a class
   public showDescription:boolean;
-  constructor( public id:number, public description:string, public author:string, public name:string){
+  constructor( public id:number, public description:string, public author:string, public name:string, public likes:number, public dislikes:number){
     this.showDescription=false
   }
 }
